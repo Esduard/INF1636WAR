f84a2028f95393d6855d1c22c7b427ee3e931d45
Esduard <42816427+Esduard@users.noreply.github.com>
2020-05-17 22:15:39 -0300

    test valdiate card

--- WarJogo-inf1636-20201/src/TEST.java
+++ WarJogo-inf1636-20201/src/TEST.java
@@ -6,53 +6,8 @@ class TEST {
 	
 	
 	
-	/*private static void testValidateAttack()
-	{
-		//public Territory(String n, Color c, ArrayList<String> neighbors );
-	    ArrayList<String> venezuela_n = new ArrayList<String>(
-	    	      Arrays.asList("Mexico","Peru","Brasil"));
-	    
-	    ArrayList<String> peru_n = new ArrayList<String>(
-	    	      Arrays.asList("Argentina","Venezuela","Brasil"));
-	    
-	    ArrayList<String> brasil_n = new ArrayList<String>(
-	    	      Arrays.asList("Venezuela","Peru","Argentina"));
-	    
-	    ArrayList<String> argentina_n = new ArrayList<String>(
-	    	      Arrays.asList("Peru","Brasil"));
-		
-		Territory Venezuela = new Territory("Venezuela",Color.Vermelho, venezuela_n);
-		Territory Peru = new Territory("Peru",Color.Azul, peru_n);
-		Territory Argentina = new Territory("Argentina",Color.Azul, argentina_n);
-		Territory Brasil = new Territory("Brasil",Color.Azul, brasil_n);
-		
-		Venezuela.setTroops(1);
-		Peru.setTroops(5);
-		Argentina.setTroops(2);
-		Brasil.setTroops(1);
-		
-		//public static boolean validateAttack(Color c,Territory src,Territory target,int n_Troops_Attacking)
-		
-		
-		System.out.println("can't attack with enemy troops");
-		System.out.println(Model.validateAttack(Color.Azul,Venezuela,Peru,1));
-		
-		System.out.println("can't attack yourself");
-		System.out.println(Model.validateAttack(Color.Azul,Brasil,Peru,1));
-		
-		System.out.println("can't do attack with no troops on standby in source");
-		System.out.println(Model.validateAttack(Color.Azul,Brasil,Venezuela,1));
-	
-		System.out.println("can't attack with more than 3 troops or 0 troops");
-		System.out.println(Model.validateAttack(Color.Azul,Peru,Venezuela,0));
-		System.out.println(Model.validateAttack(Color.Azul,Peru,Venezuela,4));
-		
-		System.out.println("verify if target is src's neighbor");
-		System.out.println(Model.validateAttack(Color.Azul,Argentina,Venezuela,1));
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("-------------TEST MODEL INTIALIZE---------------");
 		Model.testMethods();
-	}*/
+	}
 }
---
+++ WarJogo-inf1636-20201/src/model/TEST.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.util.*;
+
+public class TEST {
+	
+	
+	private static void testvalidateCardTrade()
+	{
+		System.out.println("-------------TEST CARD TRADE--------------");
+		
+		//public static boolean validateCardTrade(ArrayList<Card> selected)
+		Card []cards = new Card[9];
+		
+		cards[0]  = new Card(Shape.Triangle	, null);
+		cards[1]  = new Card(Shape.Circle	, null);
+		cards[2]  = new Card(Shape.Square	, null);
+		cards[3]  = new Card(Shape.Circle	, null);
+		cards[4]  = new Card(Shape.Square	, null);
+		cards[5]  = new Card(Shape.Triangle	, null);
+		cards[6]  = new Card(Shape.Square	, null);
+		cards[7]  = new Card(Shape.Circle	, null);
+		cards[8]  = new Card(Shape.Triangle	, null);
+		
+		System.out.println(" ");
+		
+	}
+	
+	
+	
+	
+	
+	
+	private static void testValidateAttack()
+	{
+		System.out.println("-------------TEST VALIDATE ATTACK--------------");
+		//public Territory(String n, Color c, ArrayList<String> neighbors );
+	    ArrayList<String> venezuela_n = new ArrayList<String>(
+	    	      Arrays.asList("Mexico","Peru","Brasil"));
+	    
+	    ArrayList<String> peru_n = new ArrayList<String>(
+	    	      Arrays.asList("Argentina","Venezuela","Brasil"));
+	    
+	    ArrayList<String> brasil_n = new ArrayList<String>(
+	    	      Arrays.asList("Venezuela","Peru","Argentina"));
+	    
+	    ArrayList<String> argentina_n = new ArrayList<String>(
+	    	      Arrays.asList("Peru","Brasil"));
+		
+		Territory Venezuela = new Territory("Venezuela",Color.Vermelho, venezuela_n);
+		Territory Peru = new Territory("Peru",Color.Azul, peru_n);
+		Territory Argentina = new Territory("Argentina",Color.Azul, argentina_n);
+		Territory Brasil = new Territory("Brasil",Color.Azul, brasil_n);
+		
+		Venezuela.setTroops(1);
+		Peru.setTroops(5);
+		Argentina.setTroops(2);
+		Brasil.setTroops(1);
+		
+		//public static boolean validateAttack(Color c,Territory src,Territory target,int n_Troops_Attacking)
+		
+		
+		System.out.println("can't attack with enemy troops");
+		System.out.println(Model.validateAttack(Color.Azul,Venezuela,Peru,1));
+		
+		System.out.println("can't attack yourself");
+		System.out.println(Model.validateAttack(Color.Azul,Brasil,Peru,1));
+		
+		System.out.println("can't do attack with no troops on standby in source");
+		System.out.println(Model.validateAttack(Color.Azul,Brasil,Venezuela,1));
+	
+		System.out.println("can't attack with more than 3 troops or 0 troops");
+		System.out.println(Model.validateAttack(Color.Azul,Peru,Venezuela,0));
+		System.out.println(Model.validateAttack(Color.Azul,Peru,Venezuela,4));
+		
+		System.out.println("verify if target is src's neighbor");
+		System.out.println(Model.validateAttack(Color.Azul,Argentina,Venezuela,1));
+	}
+
+}
